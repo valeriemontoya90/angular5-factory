df438345426a5ad60b20e2dbbc94dd658004667e
ValerieMontoya <valeriemontoya90@gmail.com>
2018-03-02 10:24:52 +0100

    set view matos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import {IncidentFormComponent} from "../../../../tp3/src/app/incident-form/incident-form.component";
+import {IncidentListComponent} from "../../../../tp3/src/app/incident-list/incident-list.component";
+import {MatosComponent} from "../matos/matos.component";
+
+const routes: Routes = [
+    {path: '', redirectTo: 'salles', pathMatch: 'full'},
+    {path: 'salles', component: MatosComponent}
+]
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [
     FooterComponent,
