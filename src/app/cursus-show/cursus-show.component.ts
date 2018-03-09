e946b15308e4831ae841d5bb11731fd47e7c8249
Valerie <valeriemontoya90>
2018-03-09 08:29:31 +0100

    fix last modifications

--- src/app/cursus-show/cursus-show.component.ts
+++ src/app/cursus-show/cursus-show.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {CursusModule} from '../entities/cursus.module';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CursusService} from '../services/cursus.service';
-import {CursusModule} from '../entities/cursus.module';
+import {FormationModule} from '../entities/formation.module';
+import {EventCalendar} from '../entities/eventCalendar.module';
+import {FormateurService} from '../services/formateur.service';
+import {MatiereService} from '../services/matiere.service';
+import {FormateurModule} from '../entities/formateur.module';
+import {FormationService} from '../services/formation.service';
 
 
 declare var $: any;
@@ -13,19 +20,37 @@ declare var $: any;
 export class CursusShowComponent implements OnInit {
 
     cursus = new CursusModule(-1, "", null, null,  null, null);
-    colsFormation: any[];
-    header: any;
 
-    constructor(private route: ActivatedRoute, private router: Router, private cursusService: CursusService) {
+    cursusAddForm: FormGroup;
+    colsFormation: any[];
+    headerScheduler: any;
+    eventsScheduler = [{'title' : '', 'start' : '', 'end' : ''}];
+    formations: any[];
+
+    constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router, private cursusService: CursusService,
+                private formateurService: FormateurService, private matiereService: MatiereService, private formationService: FormationService) {
     }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             const id = params['id'];
-            console.log('ID == ' + id);
+            console.log('ID == ' +  id);
             this.cursusService.getOne(id).subscribe(data => {
                 this.cursus = data;
-                console.log('cursus detail = ' + this.cursus);
+                console.log('cursus  = ' , this.cursus);
+                this.cursusAddForm = this.fb.group({
+                    'id': [this.cursus.id],
+                    'titre': [this.cursus.titre],
+                    'dateDebut': [this.cursus.dateDebut],
+                    'dateFin': [this.cursus.dateFin]
+                });
+                this.getEventCursus();
+
+                this.formationService.list().subscribe(data => {
+                    this.formations = data;
+                    console.log('formations = ',  this.formations);
+                    this.getEventsFormations();
+                });
             });
         });
 
@@ -37,10 +62,51 @@ export class CursusShowComponent implements OnInit {
             {field: 'formateur', header: 'Formateur'}
         ];
 
-        this.header = {
+        this.headerScheduler = {
             left: 'prev,next today',
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
         };
+    }
+
+    encodeCursus(cursus: CursusModule): EventCalendar {
+        let fixedDateFin = new Date(cursus.dateFin);
+        fixedDateFin.setDate(fixedDateFin.getDate()+1)
+        return {
+            title: cursus.titre,
+            start: cursus.dateDebut.toString(),
+            end: fixedDateFin.toString(),
+            backgroundColor: '#80D8FF',
+            borderColor: '#80D8FF'
+        };
+    }
+
+    encodeFormations(formation: FormationModule): EventCalendar {
+        let fixedDateFin = new Date(formation.dateFin);
+        fixedDateFin.setDate(fixedDateFin.getDate()+1)
+        return {
+            title: formation.matiere.titre,
+            start: formation.dateDebut.toString(),
+            end: fixedDateFin.toString(),
+            backgroundColor: '#FF5722',
+            borderColor: '#FF5722'
+        };
+    }
+
+    getEventCursus(): void {
+        if (this.cursus!=null) {
+            this.eventsScheduler.push(this.encodeCursus(this.cursus));
+        }
+    }
+
+    getEventsFormations(): void {
+        if (this.formations!=null) {
+            for(let formation of this.formations) {
+                if(formation.dateDebut != null && formation.dateFin){
+                    this.eventsScheduler.push(this.encodeFormations(formation));
+                }
+            }
+
+        }
     }
 }
